50dd04f495c9d3d08933997348dad076573e6dc8
Oni Benjamin <onibenjo@gmail.com>
2020-03-02 04:11:03 +0100

    Added firebase and date-fns

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-form ref="form2" class="my-5" @submit.prevent="addMessage" v-model="valid">
+    <v-text-field
+      v-model="message"
+      label="Enter your message"
+      required
+      :rules="messageRules"
+      full-width
+    ></v-text-field>
+    <v-btn
+      color="teal"
+      :disabled="!valid"
+      class="mr-4 mt-3"
+      @click="addMessage"
+    >
+      Send
+    </v-btn>
+  </v-form>
+</template>
+<script>
+import { db } from "@/firebase";
+export default {
+  props: ["name"],
+  data: () => ({
+    valid: true,
+    message: "",
+    messageRules: [v => !!v || "Message is required"]
+  }),
+  methods: {
+    async addMessage() {
+      if (this.$refs.form2.validate()) {
+        try {
+          db.collection("chat-ninja").add({
+            message: this.message,
+            name: this.name,
+            createdDate: Date.now()
+          });
+        } catch (e) {
+          console.log("error");
+        }
+        this.message = "";
+      }
+    }
+  }
+};
+</script>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,15 +1,76 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12"> {{ this.name }} - name </v-col>
-    </v-row>
+    <!-- <v-row class="text-center"> -->
+    <h2 class="teal--text text-center">Ninja Chat</h2>
+    <v-card class="mx-auto" max-width="400">
+      <!-- <v-card-text> -->
+      <v-list two-line>
+        <v-list-item-group multiple active-class="pink--text">
+          <template v-for="item in messages">
+            <v-list-item :key="item.id">
+              <v-list-item-avatar>
+                <v-avatar color="red">
+                  <span class="white--text headline">{{
+                    item.name.slice(0, 1)
+                  }}</span>
+                </v-avatar>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.name"></v-list-item-title>
+                <v-list-item-subtitle
+                  class="text--primary"
+                  v-text="item.message"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-list-item-action-text
+                  v-text="item.createdDate"
+                ></v-list-item-action-text>
+              </v-list-item-action>
+            </v-list-item>
+          </template>
+        </v-list-item-group>
+      </v-list>
+      <v-card-actions>
+        <NewMessage :name="name" />
+      </v-card-actions>
+      <!-- </v-card-text> -->
+    </v-card>
+    <!-- </v-row> -->
   </v-container>
 </template>
 
 <script>
+import NewMessage from "@/components/NewMessage";
+import { db } from "@/firebase";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 export default {
   name: "Chat",
   props: ["name"],
-  data: () => ({})
+  components: {
+    NewMessage
+  },
+  data: () => ({
+    messages: []
+  }),
+  created() {
+    let ref = db.collection("chat-ninja").orderBy("createdDate");
+
+    ref.onSnapshot(snapshot => {
+      console.log(snapshot.docChanges());
+      snapshot.docChanges().forEach(change => {
+        if (change.type == "added") {
+          let doc = change.doc;
+          this.messages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdDate: formatDistanceToNow(doc.data().createdDate)
+          });
+        }
+      });
+    });
+  }
 };
 </script>
